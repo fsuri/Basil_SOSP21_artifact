bdf2a53f716ea25db7d0caf0a0b3349beb9ac702
Andy Zhang <jz359@cornell.edu>
2019-03-26 16:07:29 -0400

    minor rule change

--- store/janusstore/Rules.mk
+++ store/janusstore/Rules.mk
@@ -3,15 +3,15 @@ d := $(dir $(lastword $(MAKEFILE_LIST)))
 # TODO replace with janus-specific stuff
 SRCS += $(addprefix $(d), client.cc shardclient.cc server.cc store.cc)
 
-PROTOS += $(addprefix $(d), tapir-proto.proto)
+PROTOS += $(addprefix $(d), janus-proto.proto)
 
 OBJS-janus-store := $(LIB-message) $(LIB-store-common) $(LIB-store-backend) \
-	$(o)tapir-proto.o $(o)store.o 
+	$(o)janus-proto.o $(o)store.o 
 
 LIB-janus-store := $(OBJS-ir-replica) $(o)server.o $(o)store.o \
-	$(o)tapir-proto.o
+	$(o)janus-proto.o
 
 LIB-janus-client := $(OBJS-ir-client)  $(LIB-udptransport) \
-	$(LIB-store-frontend) $(LIB-store-common) $(o)tapir-proto.o \
+	$(LIB-store-frontend) $(LIB-store-common) $(o)janus-proto.o \
 	$(o)shardclient.o $(o)client.o
 
