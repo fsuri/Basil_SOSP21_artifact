0db26d9e54810263bf8dfcc4e97910cbe5434302
Andy Zhang <jz359@cornell.edu>
2019-09-11 12:52:23 -0400

    implement most of txn serialization

--- store/janusstore/transaction.cc
+++ store/janusstore/transaction.cc
@@ -41,9 +41,19 @@ void Transaction::addWriteSet(const std::string &key, const std::string &value){
 	write_set[key] = value;
 }
 void Transaction::serialize(janusstore::proto::TransactionMessage *msg) const {
-	// TODO(andy): implement conversion from Transaction to TransactionMessage
 	// TODO(andy): replace with this->status when the type is updated
 	msg->set_status(janusstore::proto::TransactionMessage::Status(0));
+	msg->set_serverid(this->server_id);
+	msg->set_txnid(this->txn_id);
+	for (const auto &key : this->read_set) {
+      printf("serializing get %s\n", key.c_str());
+      msg->add_gets()->set_key(key);
+    }
+    for (const auto &pair : this->write_set) {
+      printf("serializing put %s %s\n", pair.first.c_str(), pair.second.c_str());
+      msg->add_puts()->set_key(pair.first);
+      msg->add_puts()->set_value(pair.second);
+    }
 }
 
 } // namespace janusstore
