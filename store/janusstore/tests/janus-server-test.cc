dda6e8620c6a3a17d547d1acf90b1794de2ea49d
Andy Zhang <jz359@cornell.edu>
2019-10-16 18:55:07 -0400

    more refactoring for gtest

--- store/janusstore/tests/janus-server-test.cc
+++ store/janusstore/tests/janus-server-test.cc
@@ -11,28 +11,42 @@ using std::map;
 class JanusServerTest : public  ::testing::Test
 {
 protected:
-    vector<ReplicaAddress> replicas;
+    vector<ReplicaAddress> replicaAddrs;
     std::unique_ptr<transport::Configuration> config;
     SimulatedTransport *transport;
+
+    // TODO what is this for?
+    map<int, std::vector<ReplicaAddress>> *g_replicas;
     
     janusstore::Server *server;
+    // std::unique_ptr<janusstore::Client> client;
+    std::vector<janusstore::Server*> replicas;
 
-    map<int, std::vector<ReplicaAddress>> *g_replicas;
+    int shards;
+    int replicas_per_shard;
 
-    JanusServerTest() {
-        replicas = { 
+    JanusServerTest() : shards(1), replicas_per_shard(3) {
+        replicaAddrs = { 
             { "localhost", "12345" },
             { "localhost", "12346" },
             { "localhost", "12347" }
         };
 
         config = std::unique_ptr<transport::Configuration>(
-            new transport::Configuration(3, 1, replicas));
+            new transport::Configuration(
+                replicas_per_shard, shards, replicaAddrs));
 
-        g_replicas = new std::map<int, std::vector<ReplicaAddress>>({{ 0, replicas }});
+        g_replicas = new std::map<int, std::vector<ReplicaAddress>>({{ 0, replicaAddrs }});
 
         transport = new SimulatedTransport();
-        server = new janusstore::Server(*config, 0, 0, transport);
+
+        for (int i = 0; i < replicas_per_shard; i++) {
+            auto p = new janusstore::Server(*config, 0, i, transport);
+            replicas.push_back(std::move(p));
+        }
+
+        server = replicas.front();
+        // client = std::unique_ptr<janusstore::Client>(new janusstore::Client());
     };
 
     virtual int ServerGroup() {
@@ -44,7 +58,7 @@ protected:
     }
 };
 
-TEST_F(JanusServerTest, Basic)
+TEST_F(JanusServerTest, Init)
 {
     EXPECT_EQ(ServerGroup(), 0);
     EXPECT_EQ(ServerId(), 0);
