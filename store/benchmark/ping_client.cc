75f49eeffd860fbb038c2e1fe31c13983244beda
Matt Burke <mlb452@cornell.edu>
2019-12-12 11:05:55 -0500

    update ping test

--- store/benchmark/ping_client.cc
+++ store/benchmark/ping_client.cc
@@ -1,3 +1,5 @@
+#include <random>
+
 #include <gflags/gflags.h>
 
 #include "lib/configuration.h"
@@ -8,19 +10,33 @@
 class PingClient : public TransportReceiver {
  public:
   PingClient(const transport::Configuration &config, Transport *transport,
-      uint64_t numMessages, uint64_t messageSize) :
+      uint64_t numMessages, uint64_t messageSize, const std::string &outFile,
+      uint64_t messageSizeVariance) :
       config(config), transport(transport), numMessages(numMessages),
-      messageSize(messageSize), receivedMessages(0UL) {
+      messageSize(messageSize), outFile(outFile),
+      messageSizeVariance(messageSizeVariance),
+      dist(messageSize / (1 - static_cast<double>(messageSizeVariance) / messageSize),
+          1 - static_cast<double>(messageSizeVariance) / messageSize),
+      gen(0), receivedMessages(0UL) {
     transport->Register(this, config, -1, -1);
-    for (uint64_t i = 0; i < messageSize; ++i) {
-      *pm.mutable_data() += 'a' + (i % 26);
-    }
     _Latency_Init(&rtt, "rtt");
   }
 
   virtual ~PingClient() {
     char buf[1024];
     Notice("Finished cooldown period.");
+
+    if (outFile.length() > 0) {
+      std::ofstream ofs(outFile);
+      if (ofs) {
+        for (size_t i = 0; i < latencies.size(); ++i) {
+          ofs << latencies[i] << std::endl; 
+        }
+      } else {
+        Panic("Failed to write latencies to file %s.", outFile.c_str());
+      }
+    }
+
     std::sort(latencies.begin(), latencies.end());
 
     uint64_t ns = latencies[latencies.size()/2];
@@ -50,7 +66,18 @@ class PingClient : public TransportReceiver {
     Latency_Dump(&rtt);
   }
 
+  void GenerateMessageData(std::string *data) {
+    *data = "";
+    uint64_t size = dist(gen);
+    Debug("Message size: %lu.", size);
+    for (uint64_t i = 0; i < size; ++i) {
+      *data += 'a' + (i % 26); 
+    }
+
+  }
+
   void Start() {
+    GenerateMessageData(pm.mutable_data());
     Latency_Start(&rtt);
     transport->SendMessageToReplica(this, 0, pm);
   }
@@ -62,6 +89,7 @@ class PingClient : public TransportReceiver {
     latencies.push_back(ns);
     receivedMessages++;
     if (receivedMessages < numMessages) {
+      GenerateMessageData(pm.mutable_data());
       Latency_Start(&rtt);
       transport->SendMessageToReplica(this, 0, pm);    
     } else {
@@ -74,6 +102,10 @@ class PingClient : public TransportReceiver {
   Transport *transport;
   const uint64_t numMessages;
   const uint64_t messageSize;
+  const std::string outFile;
+  const uint64_t messageSizeVariance;
+  std::binomial_distribution<uint64_t> dist;
+  std::mt19937_64 gen;
   uint64_t receivedMessages;
   PingMessage pm;
   Latency_t rtt;
@@ -83,6 +115,8 @@ class PingClient : public TransportReceiver {
 DEFINE_string(config_path, "", "path to shard configuration file");
 DEFINE_uint64(num_messages, 100, "number of messages to send and receive.");
 DEFINE_uint64(message_size, 1024, "number of bytes in each message.");
+DEFINE_string(latency_path, "", "path to latency output file");
+DEFINE_uint64(message_size_variance, 0, "variance of message size in bytes");
 
 int main(int argc, char **argv) {
   gflags::SetUsageMessage("pings a simple ping server to measure rtt times.");
@@ -97,7 +131,7 @@ int main(int argc, char **argv) {
   transport::Configuration config(configStream);
   TCPTransport transport(0.0, 0.0, 0, false);
   PingClient *client = new PingClient(config, &transport, FLAGS_num_messages,
-      FLAGS_message_size);
+      FLAGS_message_size, FLAGS_latency_path, FLAGS_message_size_variance);
   transport.Timer(0, [client]() { client->Start(); });
 
   transport.Run();
