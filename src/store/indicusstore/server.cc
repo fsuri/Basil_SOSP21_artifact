3afa8b771aa882285ee729ad557f0d1860cfe263
Matt Burke <mlb452@cornell.edu>
2020-03-20 19:51:19 -0400

    add req_id to Phase2Reply

--- src/store/indicusstore/server.cc
+++ src/store/indicusstore/server.cc
@@ -256,6 +256,7 @@ void Server::HandlePhase2(const TransportAddress &remote,
   }
 
   proto::Phase2Reply reply;
+  reply.set_req_id(msg.req_id());
   reply.set_decision(decision); 
   p2Decisions[msg.txn_id()] = decision;
 
