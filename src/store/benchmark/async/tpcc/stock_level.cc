614d5bd132dc44d0a7e57b9722ce8cfab43826d2
Daniel Weber <dw475@cornell.edu>
2020-04-16 21:29:40 -0500

    Fixed typo

--- src/store/benchmark/async/tpcc/stock_level.cc
+++ src/store/benchmark/async/tpcc/stock_level.cc
@@ -11,7 +11,7 @@ namespace tpcc {
 StockLevel::StockLevel(uint32_t w_id, uint32_t d_id, std::mt19937 &gen) :
     w_id(w_id), d_id(d_id) {
   min_quantity = std::uniform_int_distribution<uint8_t>(10, 20)(gen);
-}``
+}
 
 StockLevel::~StockLevel() {
 }
