5f50edf71a2d6d5767d8834802d4176946e674cd
Daniel Weber <dantech88@yahoo.com>
2020-04-28 02:51:12 -0500

    added threadpool

---
+++ src/lib/threadpool.cc
@@ -0,0 +1,58 @@
+#include "lib/threadpool.h"
+
+ThreadPool::ThreadPool(uint32_t numThreads) {
+  running = true;
+  for (int i = 0; i < numThreads; i++) {
+    std::thread t([this] {
+      while (true) {
+        std::pair<std::function<void*()>, EventInfo*> job;
+        {
+          // only acquire the lock in this block so that the
+          // function execution is not holding the lock
+          std::lock_guard<std::mutex> lock(this->worklistMutex);
+          cv.wait(lk, [this] { return this->worklist.size() > 0 || !running; });
+          if (!running) {
+            break;
+          }
+          if (this->worklist.size() == 0) {
+            continue;
+          }
+          job = this->worklist.front();
+          this->worklist.pop_front();
+        }
+
+        job.second->r = job.first();
+        // This _should_ be thread safe
+        event_active(job.second->ev, 0, 0);
+      }
+    });
+    t.detach();
+  }
+}
+
+void ThreadPool::stop() {
+  running = false;
+  cv.notify_all();
+}
+
+
+static void ThreadPool::EventCallback(evutil_socket_t fd, short what, void *arg) {
+  // we want to run the callback in the main event loop
+  EventInfo* info = (EventInfo*) arg;
+  info->cb(info->r);
+  event_free(info->ev);
+  delete info;
+}
+
+void ThreadPool::dispatch(std::function<void*()> f, std::function<void(void*)> cb, event_base* libeventBase) {
+  EventInfo* info = new EventInfo();
+  info->cb = cb;
+  info->ev = event_new(libeventBase, -1, 0, ThreadPool::EventCallback, info);
+  event_add(info->ev, NULL);
+
+  pair<function<void*()>, EventInfo*> job(f, info);
+
+  std::lock_guard<std::mutex> lk(worklistMutex);
+  worklist.push_back(job);
+  cv.notify_one();
+}
