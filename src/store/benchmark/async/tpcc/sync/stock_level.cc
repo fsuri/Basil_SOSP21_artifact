fe15ddbad658d61151cc47a8aa46d0f8ab5d1ddd
Matt Burke <mlb452@cornell.edu>
2020-04-30 20:04:49 -0400

    temp remove committed read insert lat measurement

--- src/store/benchmark/async/tpcc/sync/stock_level.cc
+++ src/store/benchmark/async/tpcc/sync/stock_level.cc
@@ -31,7 +31,6 @@ transaction_status_t SyncStockLevel::Execute(SyncClient &client) {
   uint32_t next_o_id = d_row.next_o_id();
   Debug("Orders: %u-%u", next_o_id - 20, next_o_id - 1);
 
-  std::map<uint32_t, StockRow> stockRows;
   for (size_t ol_o_id = next_o_id - 20; ol_o_id < next_o_id; ++ol_o_id) {
     Debug("Order %lu", ol_o_id);
     client.Get(OrderRowKey(w_id, d_id, ol_o_id), timeout);
@@ -39,17 +38,17 @@ transaction_status_t SyncStockLevel::Execute(SyncClient &client) {
 
   client.Wait(strs);
 
-  std::vector<uint32_t> ol_cnts;
-  for (size_t ol_o_id = next_o_id - 20; ol_o_id < next_o_id; ++ol_o_id) {
-    if (str.empty()) {
-      Debug("  Non-existent Order %lu", ol_o_id);
+  std::map<uint32_t, uint32_t> ol_cnts;
+  for (uint32_t ol_o_id = next_o_id - 20; ol_o_id < next_o_id; ++ol_o_id) {
+    if (strs[ol_o_id + 20 - next_o_id].empty()) {
+      Debug("  Non-existent Order %u", ol_o_id);
       continue;
     }
     OrderRow o_row;
     UW_ASSERT(o_row.ParseFromString(strs[ol_o_id + 20 - next_o_id]));
     Debug("  Order Lines: %u", o_row.ol_cnt());
 
-    ol_cnts.push_back(o_row.ol_cnt());
+    ol_cnts[ol_o_id] = o_row.ol_cnt();
     for (size_t ol_number = 0; ol_number < o_row.ol_cnt(); ++ol_number) {
       Debug("    OL %lu", ol_number);
       std::string ol_key = OrderLineRowKey(w_id, d_id, ol_o_id, ol_number);
@@ -60,11 +59,15 @@ transaction_status_t SyncStockLevel::Execute(SyncClient &client) {
   strs.clear();
   client.Wait(strs);
 
+  std::map<uint32_t, StockRow> stockRows;
   size_t strsIdx = 0;
-  for (size_t ol_o_id = next_o_id - 20; ol_o_id < next_o_id; ++ol_o_id) {
-    for (size_t ol_number = 0; ol_number < ol_cnts[ol_o_id + 20 - next_o_id]; ++ol_number) {
+  for (const auto order_cnt : ol_cnts) {
+    Debug("Order %u", order_cnt.first);
+    for (size_t ol_number = 0; ol_number < order_cnt.second; ++ol_number) {
       OrderLineRow ol_row;
-      if (str.empty()) {
+      Debug("  OL %lu", ol_number);
+      Debug("  Total OL index: %lu.", strsIdx);
+      if (strs[strsIdx].empty()) {
         Debug("  Non-existent Order Line %lu", ol_number);
         continue;
       }
@@ -78,6 +81,7 @@ transaction_status_t SyncStockLevel::Execute(SyncClient &client) {
     }
   }
 
+  strs.clear();
   client.Wait(strs);
 
   Debug("COMMIT");
