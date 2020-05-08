19f56d4e6726b6c04790fbcb3b0b054457348264
Matt Burke <mlb452@cornell.edu>
2020-05-08 01:38:17 -0400

    update jsons

--- src/store/common/pinginitiator.cc
+++ src/store/common/pinginitiator.cc
@@ -1,7 +1,67 @@
 #include "store/common/pinginitiator.h"
 
-PingInitiator::PingInitiator(Transport *transport) : transport(transport) {
+#include "lib/message.h"
+
+PingInitiator::PingInitiator(Transport *transport, size_t numReplicas) :
+    transport(transport), numReplicas(numReplicas), alpha(0.75), length(5000) {
 }
 
 PingInitiator::~PingInitiator() {
+}
+
+void PingInitiator::Start(TransportReceiver *receiver) {
+  for (size_t i = 0; i < numReplicas; ++i) {
+    SendPing(receiver, i);
+  }
+  transport->Timer(length, [this](){
+    done = true;
+  });
+}
+
+void PingInitiator::SendPing(TransportReceiver *receiver, size_t replica) {
+  ping.set_salt(rd());
+  struct timespec start;
+  if (clock_gettime(CLOCK_MONOTONIC, &start) < 0) {
+    PPanic("Failed to get CLOCK_MONOTONIC");
+  }
+  outstandingSalts.insert(std::make_pair(ping.salt(), std::make_pair(replica, start)));
+  transport->SendMessageToReplica(receiver, 0, replica, ping);
+}
+
+void PingInitiator::HandlePingResponse(TransportReceiver *receiver,
+    const TransportAddress &remote, const PingMessage &ping) {
+  auto saltItr = outstandingSalts.find(ping.salt());
+  if (saltItr != outstandingSalts.end()) {
+    struct timespec now;
+    if (clock_gettime(CLOCK_MONOTONIC, &now) < 0) {
+      PPanic("Failed to get CLOCK_MONOTONIC");
+    }
+
+    uint64_t roundTrip = timespec_delta(saltItr->second.second, now);
+    auto estimateItr = roundTripEstimates.find(saltItr->second.first);
+    if (estimateItr == roundTripEstimates.end()) {
+      roundTripEstimates.insert(std::make_pair(saltItr->second.first, roundTrip));
+    } else {
+      estimateItr->second = (1 - alpha) * estimateItr->second + alpha * roundTrip; 
+    }
+
+    if (!done) {
+      SendPing(receiver, saltItr->second.first);
+    }
+    outstandingSalts.erase(saltItr);
+  }
+}
+
+uint64_t PingInitiator::timespec_delta(const struct timespec &a,
+      const struct timespec &b) {
+  uint64_t delta;
+  delta = b.tv_sec - a.tv_sec;
+  delta *= 1000000000ll;
+  if (b.tv_nsec < a.tv_nsec) {
+    delta -= 1000000000ll;
+    delta += (b.tv_nsec + 1000000000ll) - a.tv_nsec;
+  } else {
+    delta += b.tv_nsec - a.tv_nsec;
+  }
+  return delta;
 }
--- src/store/common/pinginitiator.h
+++ src/store/common/pinginitiator.h
@@ -1,20 +1,37 @@
 #ifndef PING_INITIATOR_H
 #define PING_INITIATOR_H
 
+#include <ctime>
 #include <map>
 #include <random>
 
 #include "lib/transport.h"
+#include "store/common/common-proto.pb.h"
 
 class PingInitiator {
  public:
-  PingInitiator(Transport *transport);
+  PingInitiator(Transport *transport, size_t numReplicas);
   virtual ~PingInitiator();
 
+  void Start(TransportReceiver *receiver);
+  void SendPing(TransportReceiver *receiver, size_t replica);
+  void HandlePingResponse(TransportReceiver *receiver, 
+      const TransportAddress &remote, const PingMessage &ping);
+
  private:
+  static uint64_t timespec_delta(const struct timespec &a,
+      const struct timespec &b);
   Transport *transport;
+  const size_t numReplicas;
+  const double alpha;
+  const uint64_t length;
+
+  bool done;
   std::random_device rd;
-  std::map<uint64_t, uint64_t> outstandingSalts;
+  std::map<uint64_t, std::pair<size_t, struct timespec>> outstandingSalts;
+  std::map<size_t, uint64_t> roundTripEstimates;
+
+  PingMessage ping;
 };
 
 #endif /* PING_INITIATOR_H */
