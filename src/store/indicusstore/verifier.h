971fb90599ccddb77d4adc78b502b260520a5a00
Florian Suri-Payer <fs435@cornell.edu>
2020-08-10 09:55:49 -0400

    ...

--- src/store/indicusstore/verifier.h
+++ src/store/indicusstore/verifier.h
@@ -28,7 +28,7 @@ class Verifier {
 
   virtual void asyncBatchVerify(crypto::PubKey *publicKey, const std::string &message,
       const std::string &signature, verifyCallback vb, bool multithread, bool autocomplete = false) = 0;
-      
+
   virtual void Complete(bool multithread, bool force_complete = false) = 0;
 };
 
