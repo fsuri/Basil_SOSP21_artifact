56f7c29678e2165c867432859439f9fc0423957b
Yunhao <yz2327@cornell.edu>
2020-11-08 18:19:39 -0500

    scripts

--- src/scripts/hotstuff_server.sh
+++ src/scripts/hotstuff_server.sh
@@ -8,3 +8,4 @@ export DEBUG=store/hotstuffstore/*; store/server --config_path shard-r4.config -
 
 
 export DEBUG=store/hotstuffstore/*; store/server --config_path shard-r4.config --group_idx 0 --num_groups 1 --num_shards 1 --replica_idx 3 --protocol hotstuff --num_keys 1 --debug_stats --indicus_key_path keys&
+
