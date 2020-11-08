3b0051947f150fbbc7a321c19028e69f8ab9249f
Yunhao <yz2327@cornell.edu>
2020-11-08 18:46:24 -0500

    add bash comment

--- src/scripts/hotstuff_client.sh
+++ src/scripts/hotstuff_client.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 store/benchmark/async/benchmark --client_id 0 --config_path shard-r4.config --num_groups 1 --num_shards 1 --protocol_mode hotstuff --num_keys 1 --benchmark rw --num_ops_txn 2 --exp_duration 10 --warmup_secs 0 --cooldown_secs 0 --key_selector zipf --zipf_coefficient 0.0 --stats_file "stats-0.json" --indicus_key_path scripts/local_keys &
 
 
