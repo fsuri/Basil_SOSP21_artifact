6fb72bb2f617ee5847870f2aaf9efb6942934348
Yunhao <yz2327@cornell.edu>
2020-11-08 19:07:11 -0500

    update script format

--- src/scripts/hotstuff_server.sh
+++ src/scripts/hotstuff_server.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-export DEBUG=store/hotstuffstore/*; store/server --config_path shard-r4.config --group_idx 0 --num_groups 1 --num_shards 1 --replica_idx 0 --protocol hotstuff --num_keys 1 --debug_stats --indicus_key_path scripts/local_keys&
+export DEBUG=store/hotstuffstore/*
 
-export DEBUG=store/hotstuffstore/*; store/server --config_path shard-r4.config --group_idx 0 --num_groups 1 --num_shards 1 --replica_idx 1 --protocol hotstuff --num_keys 1 --debug_stats --indicus_key_path scripts/local_keys&
+store/server --config_path shard-r4.config --group_idx 0 --num_groups 1 --num_shards 1 --replica_idx 0 --protocol hotstuff --num_keys 1 --debug_stats --indicus_key_path scripts/local_keys&
+
+export DEBUG=store/hotstuffstore/*
+
+store/server --config_path shard-r4.config --group_idx 0 --num_groups 1 --num_shards 1 --replica_idx 1 --protocol hotstuff --num_keys 1 --debug_stats --indicus_key_path scripts/local_keys&
 
 
-export DEBUG=store/hotstuffstore/*; store/server --config_path shard-r4.config --group_idx 0 --num_groups 1 --num_shards 1 --replica_idx 2 --protocol hotstuff --num_keys 1 --debug_stats --indicus_key_path scripts/local_keys&
+export DEBUG=store/hotstuffstore/*
+
+store/server --config_path shard-r4.config --group_idx 0 --num_groups 1 --num_shards 1 --replica_idx 2 --protocol hotstuff --num_keys 1 --debug_stats --indicus_key_path scripts/local_keys&
 
 
-export DEBUG=store/hotstuffstore/*; store/server --config_path shard-r4.config --group_idx 0 --num_groups 1 --num_shards 1 --replica_idx 3 --protocol hotstuff --num_keys 1 --debug_stats --indicus_key_path scripts/local_keys&
+export DEBUG=store/hotstuffstore/*
+
+store/server --config_path shard-r4.config --group_idx 0 --num_groups 1 --num_shards 1 --replica_idx 3 --protocol hotstuff --num_keys 1 --debug_stats --indicus_key_path scripts/local_keys&
 
 # node18-ntp-hotstuff
