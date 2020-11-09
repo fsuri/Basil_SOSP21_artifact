ddd7e7701e03e5af5e679b2504c6494935db335a
Yunhao <yz2327@cornell.edu>
2020-11-08 19:37:56 -0500

    remove sleep time, no use

--- src/scripts/hotstuff_server.sh
+++ src/scripts/hotstuff_server.sh
@@ -4,17 +4,14 @@ export DEBUG=store/hotstuffstore/*
 
 store/server --config_path shard-r4.config --group_idx 0 --num_groups 1 --num_shards 1 --replica_idx 0 --protocol hotstuff --num_keys 1 --debug_stats --indicus_key_path scripts/local_keys&
 
-sleep 0.5
 export DEBUG=store/hotstuffstore/*
 
 store/server --config_path shard-r4.config --group_idx 0 --num_groups 1 --num_shards 1 --replica_idx 1 --protocol hotstuff --num_keys 1 --debug_stats --indicus_key_path scripts/local_keys&
 
-sleep 0.5
 export DEBUG=store/hotstuffstore/*
 
 store/server --config_path shard-r4.config --group_idx 0 --num_groups 1 --num_shards 1 --replica_idx 2 --protocol hotstuff --num_keys 1 --debug_stats --indicus_key_path scripts/local_keys&
 
-sleep 0.5
 export DEBUG=store/hotstuffstore/*
 
 store/server --config_path shard-r4.config --group_idx 0 --num_groups 1 --num_shards 1 --replica_idx 3 --protocol hotstuff --num_keys 1 --debug_stats --indicus_key_path scripts/local_keys&
