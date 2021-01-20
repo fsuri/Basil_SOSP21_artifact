0ac6855eefa64d38aaeffcf9b7e2d43b99e0fa0e
Yunhao <yz2327@cornell.edu>
2021-01-20 16:51:13 -0500

    more elegant batch_size.sh

--- src/scripts/batch_size.sh
+++ src/scripts/batch_size.sh
@@ -1,5 +1,15 @@
+if [ "$#" -le 0 ]
+then
+
+  echo "Usage: batch_size.sh {batch size}"
+
+else
+
+
 ls config | while read shard
 do
     echo modifying ${shard} for batch size $1
     sed -i "1c\block-size = ${1}" ./config/${shard}/hotstuff.gen.conf
 done
+
+fi
